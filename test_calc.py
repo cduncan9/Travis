db9bf32b9ea77a4d4044e64a6d4711e6494fde4d
Canyon Duncan <canyon.duncan@duke.edu>
2020-05-27 10:37:56 -0400

    added divide unit test

--- test_calc.py
+++ test_calc.py
@@ -19,3 +19,10 @@ def test_multiply():
     answer = multiply(4, 5)
     expected = 20
     assert answer == expected
+    
+    
+def test_divide():
+    from calc import divide
+    answer = divide(4, 2)
+    expected = 2
+    assert answer == expected

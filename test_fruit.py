a9e2585e7854aabf07ea0c54620a1eef5a3fb36c
Canyon Duncan <canyon.duncan@duke.edu>
2020-05-28 15:04:59 -0400

    more pep8 fixes

--- test_fruit.py
+++ test_fruit.py
@@ -3,8 +3,8 @@ import pytest
 
 @pytest.mark.parametrize("in_fruit, expected",
                          [("apple", True),
-                         ("banana ", True),
-                         ("wood", False)])
+                          ("banana ", True),
+                          ("wood", False)])
 def test_is_fruit(in_fruit, expected):
     from fruit import is_fruit
     answer = is_fruit(in_fruit)
